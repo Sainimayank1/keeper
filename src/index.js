ac61fd2c7c092e3a24eb25e4016e31ddb5d342a5
Mayank <mayanksaini4455@gmail.com>
2022-08-25 14:46:54 +0530

    Adding impornt files like APP,Header and FOOTER . also gives the basic structure for runing our react file .

--- src/index.js
+++ src/index.js
@@ -9,4 +9,13 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in teh styles.css file to appy styling
+
+
+import React from "react"
+import Reactdom from "react-dom"
+import App from "./Collections/App"
+
+
+
+Reactdom.render(<App />,document.getElementById("root"))
